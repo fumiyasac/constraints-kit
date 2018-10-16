d89c596fe3fd58f3f4277a30281918d2f009e87a
Astemir Eleev <astemireleev@gmail.com>
2018-10-16 14:35:07 +0300

    Initial Commit

---
+++ constraints-kit/constraints_kit.h
@@ -0,0 +1,19 @@
+//
+//  constraints_kit.h
+//  constraints-kit
+//
+//  Created by Astemir Eleev on 16/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for constraints_kit.
+FOUNDATION_EXPORT double constraints_kitVersionNumber;
+
+//! Project version string for constraints_kit.
+FOUNDATION_EXPORT const unsigned char constraints_kitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <constraints_kit/PublicHeader.h>
+
+
