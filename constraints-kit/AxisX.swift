34d12484fa609a6995554f47234bf691e68db231
Astemir Eleev <astemireleev@gmail.com>
2018-11-06 15:14:13 +0300

    Created AxisX enum type

---
+++ constraints-kit/AxisX.swift
@@ -0,0 +1,28 @@
+//
+//  AxisX.swift
+//  constraints-kit
+//
+//  Created by Astemir Eleev on 06/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public enum AxisX {
+    case left, right
+}
+
+public enum AxisEdge {
+    case leading, trailing
+}
+
+internal extension AxisEdge {
+    func convert() -> AxisX {
+        switch self {
+        case .leading:
+            return .left
+        case .trailing:
+            return .right
+        }
+    }
+}
