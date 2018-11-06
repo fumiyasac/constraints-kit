fa17591dcc47652a9cbaf907907dcc779cf7b7c7
Astemir Eleev <astemireleev@gmail.com>
2018-11-06 15:14:06 +0300

    Created AxisY enum type

---
+++ constraints-kit/AxisY.swift
@@ -0,0 +1,13 @@
+//
+//  AxisY.swift
+//  constraints-kit
+//
+//  Created by Astemir Eleev on 06/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+public enum AxisY {
+    case top, bottom
+}
