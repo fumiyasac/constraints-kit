2d72674ff5106e1f3875033b95136bae365bc041
Astemir Eleev <astemireleev@gmail.com>
2018-11-08 17:14:51 +0300

    Created UILayoutArea struct [in development]

---
+++ constraints-kit/UILayoutArea.swift
@@ -0,0 +1,33 @@
+//
+//  UILayoutArea.swift
+//  constraints-kit
+//
+//  Created by Astemir Eleev on 08/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+//public struct UILayoutArea: OptionSet {
+//
+//    public let rawValue: UInt
+//
+//    public static let topLeft = UILayoutArea(rawValue: 1 << 0)
+//    public static let topRight = UILayoutArea(rawValue: 1 << 1)
+//    public static let topCenter = UILayoutArea(rawValue: 1 << 2)
+//    public static let midLeft = UILayoutArea(rawValue: 1 << 3)
+//    public static let midRight = UILayoutArea(rawValue: 1 << 4)
+//    public static let bottomLeft = UILayoutArea(rawValue: 1 << 5)
+//    public static let bottomRight = UILayoutArea(rawValue: 1 << 6)
+//    public static let bottomCenter = UILayoutArea(rawValue: 1 << 7)
+//
+//    public static let allCases: [UILayoutArea] = [ .topLeft, .topRight, .topCenter, .midLeft, .midRight, .bottomLeft, .bottomRight, .bottomCenter]
+//
+//    public init(rawValue: RawValue) {
+//        self.rawValue = rawValue
+//    }
+//
+//    internal func convert() -> [AxisType] {
+//
+//    }
+//}
