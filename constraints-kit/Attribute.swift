929216737b14cf7da8f1a6a15fcbb18e53158c96
Astemir Eleev <astemireleev@gmail.com>
2018-11-09 12:00:36 +0300

    Added new case called `.notAnAttribute`

--- constraints-kit/Attribute.swift
+++ constraints-kit/Attribute.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 public enum Attribute {
-    case top, bottom, left, right, aspect, width, height, centerX, centerY, lastBaseline, firstBaseline, leading, trailing
+    case top, bottom, left, right, aspect, width, height, centerX, centerY, lastBaseline, firstBaseline, leading, trailing, notAnAttribute
 }
 
-internal extension Attribute {
+extension Attribute {
     
     func convert() -> NSLayoutConstraint.Attribute {
         switch self {
@@ -42,6 +42,39 @@ internal extension Attribute {
             return .trailing
         default:
             return .notAnAttribute
+        }
+    }
+    
+    func toAxisX() -> AxisX? {
+        switch self {
+        case .left:
+            return AxisX.left
+        case .right:
+            return AxisX.right
+        default:
+            return nil
+        }
+    }
+    
+    func toAxisY() -> AxisY? {
+        switch self {
+        case .top:
+            return AxisY.top
+        case .bottom:
+            return AxisY.bottom
+        default:
+            return nil
+        }
+    }
+    
+    func toAxis() -> Axis? {
+        switch self {
+        case .centerX:
+            return Axis.horizontal
+        case .centerY:
+            return Axis.vertical
+        default:
+            return nil
         }
     }
 }
