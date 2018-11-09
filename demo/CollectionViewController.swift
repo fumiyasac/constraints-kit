6f8e3510eedf12e1d7feecfd6eea85548557625f
Astemir Eleev <astemireleev@gmail.com>
2018-11-09 19:02:15 +0300

    Created CollectionViewController class

---
+++ demo/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  demo
+//
+//  Created by Astemir Eleev on 09/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
